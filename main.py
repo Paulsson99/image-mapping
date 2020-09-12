ac45c9f1c2f0139b50facbdf952105fe8a460724
Simon Paulsson <simon@paulsson.one>
2020-09-12 19:49:41 +0200

    can save a gif now

--- main.py
+++ main.py
@@ -3,29 +3,81 @@ import numpy as np
 
 from transforms import *
 
+from debug import timer
+
 IMAGE_FILE = 'images/py_logo.png'
-OUTPUT_FILE = 'images/transformed.png'
+OUTPUT_FILE = 'images/transformed.gif'
+
+FRAME_COUNT = 100
+DURATION = 40 # ms
+
+BACKGROUND = np.array((0, 0, 0))
 
 def load_image(filename):
-	# Load the image as a numpy array
+	# Load the image as a numpy array (saved as [y, x, RGBA])
 	return np.array(Image.open(filename).convert('RGBA'))
 
-def save_image(im, filename):
-	# Save the image
-	im.save(filename)
+def save_image_sequence(im_sequence, filename):
+	images = []
+	for im in im_sequence:
+		images.append(Image.fromarray(blend_background(im, BACKGROUND)))
+	images[0].save(filename, save_all=True, append_images=images[1:], optimize=False, duration=DURATION, loop=1)
 
+def get_grid(x_size, y_size):
+	x = np.arange(x_size)
+	y = np.arange(y_size)
+	xx, yy = np.meshgrid(x, y)
+	return np.column_stack((xx.flatten(), yy.flatten()))
+	#return np.array([(x, y) for x in range(x_size) for y in range(y_size)], dtype=int)
+
+@timer
 def transform_image(im, f):
-	im_shape = im.shape
-	im_transform = np.zeros(im_shape, dtype=im.dtype)
+	y_size, x_size, color_size = im.shape
 
-	for x in range(im_shape[0]):
-		for y in range(im_shape[1]):
-			xx, yy = f(x, y)
-			xx, yy = int(np.round(xx)), int(np.round(yy))
-			if 0 <= xx < im_shape[0] and 0 <= yy < im_shape[1]:
-				im_transform[xx, yy, :] = im[x, y, :]
+	# Create a grid of points for every pixel in the image
+	grid = get_grid(x_size, y_size)
 
-	return im_transform
+	# Transform the grid
+	transformed_grid = f(grid)
+	
+	im_sequence = np.zeros((FRAME_COUNT, y_size, x_size, color_size), dtype=im.dtype)
+	for frame in range(FRAME_COUNT):
+		# Linear interpolation between grid and transformed grid
+		t = frame / (FRAME_COUNT - 1) if FRAME_COUNT > 1 else 1
+		frame_grid = lerp(grid, transformed_grid, t)
+
+		# Round to closest pixel coordinate
+		frame_grid = np.round(frame_grid).astype(int)
+
+		# Mask for all corinates within the image border
+		coord_in_range = valid_coordinates(frame_grid, 0, x_size, 0, y_size)
+
+		valid_transform = frame_grid[coord_in_range,:]
+		valid_grid = grid[coord_in_range,:]
+
+		# Set the new pixel colors
+		im_sequence[frame, valid_transform[:,1], valid_transform[:,0],:] = im[valid_grid[:,1], valid_grid[:,0], :]
+
+	return im_sequence
+
+def transform_grid(grid, f):
+	# Transform a grid with the function f and round to closest pixel
+	return f(grid)
+
+def valid_coordinates(grid, min_x, max_x, min_y, max_y):
+	x_coord_in_range = np.logical_and(min_x <= grid[...,0], grid[...,0] < max_x)
+	y_coord_in_range = np.logical_and(min_y <= grid[...,1], grid[...,1] < max_y)
+	return np.logical_and(x_coord_in_range, y_coord_in_range)
+
+def lerp(grid0, grid1, t):
+	return grid0 + (grid1 - grid0) * t
+
+def blend_background(colors, bg):
+	alpha = (colors[...,3] / 255)[...,np.newaxis]
+	new_color = colors[...,:3] * alpha + (1 - alpha) * bg
+	return np.round(new_color).astype(np.uint8)
+
+
 
 
 
@@ -34,16 +86,19 @@ def main():
 	im = load_image(IMAGE_FILE)
 
 	# Transform the image
-	im_transform = transform_image(im, mirror((1, 1), (im.shape[0] / 2, im.shape[1] / 2)))
-
-	print(im_transform.shape)
-
-	# Convert a numpy array into an image object
-	pil_img = Image.fromarray(im_transform)
+	#transform_func = mirror((1, 0), (0, im.shape[1] / 2))
+	transform_func = möbius(0, 512, 512, 512+512j, 512+512j, 512j)
+	#transform_func = mirror((1, 1), (im.shape[0] / 2, im.shape[1] / 2))
+	print('Calculation transformation...')
+	im_sequence = transform_image(im, transform_func)
 
 	# Save
-	save_image(pil_img, OUTPUT_FILE)
+	print('Saving image to {}'.format(OUTPUT_FILE))
+	save_image_sequence(im_sequence, OUTPUT_FILE)
+	print('Done')
 
+	# Convert a numpy array into an image object
+	pil_img = Image.fromarray(im_sequence[-1])
 	# Show image
 	pil_img.show()
 
--- transforms.py
+++ transforms.py
@@ -1,15 +1,63 @@
 import numpy as np
+from debug import timer
 
 # TODO: Decorator?
 
 
 
 def mirror(line, origin):
-	line = np.array(line, dtype=np.float64)
+	# Normalize the line
+	line = np.array(line, dtype=np.float64) / np.sqrt(np.dot(line, line))
 	origin = np.array(origin, dtype=np.float64)
-	def mirror_func(x, y):
-		p = np.array((x, y), dtype=np.float64)
-		p -= origin
-		p = 2 * np.dot(line, p) / np.dot(line, line) * line - p
-		return p + origin
+	def mirror_func(grid):
+		grid = grid.astype(float)
+		grid -= origin
+		s = 2 * np.matmul(grid, line)
+		grid = np.matmul(s[:,np.newaxis], line[np.newaxis,:]) - grid
+		return grid + origin
 	return mirror_func
+
+
+def möbius(z1, w1, z2, w2, z3, w3):
+	# Calculate the möbius function h(z) defined by the cross ratio [z, z1, z2, z3]
+	h_a, h_b, h_c, h_d = cross_ratio(z1, z2, z3)
+	# Calculate the möbius function g(w) defined by the cross ratio [w, w1, w2, w3]
+	g_a, g_b, g_c, g_d = cross_ratio(w1, w2, w3)
+	# Calculate the composition of g^(-1)(f(z))
+	a, b, c, d = möbius_composition(h_a, h_b, h_c, h_d, g_d, -g_b, -g_c, g_a)
+
+	def möbius_func(grid):
+		# Convert the grid to complex numbers
+		Z = grid[:, 0] + grid[:, 1] * 1j
+		# Möbius tansformation
+		Z = (a * Z + b) / (c * Z + d)
+		# Convert to a grid again
+		return np.column_stack((np.real(Z), np.imag(Z)))
+
+	return möbius_func
+
+def cross_ratio(z1, z2, z3):
+	a = z2 - z3
+	b = -z1 * (z2 - z3)
+	c = z2 - z1
+	d = -z3 * (z2 - z1)
+	return (a, b, c, d)
+
+def möbius_composition(a, b, c, d, e, f, g, h):
+	a_ = a * e + c * f
+	b_ = b * e + d * f
+	c_ = a * g + c * h
+	d_ = b * g + d * h
+	return (a_, b_, c_, d_)
+
+
+
+
+
+
+
+
+
+
+
+
